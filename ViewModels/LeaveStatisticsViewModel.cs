76e5dc50f3201566b5211f89fb0dd1366661af24
luonganhduc3103 <laduc.dhti15a9hn@sv.uneti.edu.vn>
2025-01-04 21:51:47 +0700

    update_by_duc

---
+++ ViewModels/LeaveStatisticsViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Web_DonNghiPhep.ViewModels
+{
+    public class LeaveStatisticsViewModel
+    {
+        public string EmployeeId { get; set; }
+        public string EmployeeName { get; set; }
+        public string DepartmentName { get; set; }
+        public int RemainingLeaveDays { get; set; }
+        public int UsedLeaveDays { get; set; }
+    }
+}
